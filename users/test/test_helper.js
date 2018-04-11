50e8fb7e197f999386951b72ca5a11b47f6237a8
Malaxor <nogobaduk@gmail.com>
2018-04-10 21:51:25 -0700

    mocha testing

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,9 +1,21 @@
 
 const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost/users_test");
-mongoose.connection
-.once("open", () => console.log("Good to go!"))
-.on("error", (error) => {
-	console.warn("Warning", error);
+before(() => {
+
+	mongoose.connect("mongodb://localhost/users_test");
+	mongoose.connection
+	.once("open", () => {})
+	.on("error", (error) => {
+		console.warn("Warning", error);
+	});
 });
+
+beforeEach((done) => {
+
+	mongoose.connection.collections.users.drop(() => {
+		// Ready to run the next test
+		done();
+	});
+});	
