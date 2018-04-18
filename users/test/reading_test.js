0753d8d0082b3da48a002953b44b3a9a7bb2abfe
Malaxor <nogobaduk@gmail.com>
2018-04-17 23:34:18 -0700

    query modifiers: sort, skip and limit

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -4,12 +4,16 @@ const User   = require('../src/user');
 
 describe('Reading users out of the database', () => {
 
-	let joe;
+	let joe, maria, alex, zach;
 
 	beforeEach((done) => {
 
-		joe = new User({name: 'Joe'});
-		joe.save()
+		alex  = new User({ name: 'Alex' });
+		joe   = new User({ name: 'Joe' });
+		maria = new User({ name: 'Maria' });
+		zach  = new User({ name: 'Zach' });
+		
+		Promise.all([ alex.save(), joe.save(), maria.save(), zach.save() ])
 		.then(() => done());
 	});	
 
@@ -27,6 +31,22 @@ describe('Reading users out of the database', () => {
 		User.findOne({ _id: joe._id })
 		.then((user) => {
 			assert(user.name === 'Joe');
+			done();
+		});
+	});
+
+	it('can skip and limit the result set', (done) => {
+
+		User.find({})
+		// sort ascending
+		.sort({ name: 1 })
+		.skip(1)
+		.limit(2)
+		.then((users) => {
+
+			assert(users.length === 2);
+			assert(users[0].name === 'Joe');
+			assert(users[1].name === 'Maria');
 			done();
 		});
 	});
