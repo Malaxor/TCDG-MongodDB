ea57d12fdec88b587437e77dca4efa9eff42285c
Malaxor <nogobaduk@gmail.com>
2018-04-17 19:26:23 -0700

    tested deeply nested associations

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -13,7 +13,7 @@ describe('Assocations', () => {
 
 	joe = new User({ name: 'Joe' });
 	blogPost = new BlogPost({ title: 'JS is Great', content: 'Yep it really is' });
-	comment = new Comment({ content: 'Congrats on great post' });
+	comment = new Comment({ content: 'Congrats on a great post' });
 
 	joe.blogPosts.push(blogPost);
 	blogPost.comments.push(comment);
@@ -25,11 +25,33 @@ describe('Assocations', () => {
 
   it('saves a relation between a user and a blogpost', (done) => {
 
-    User.findOne({ name: 'Joe' })
-      .populate('blogPosts')
-      .then((user) => {
-        assert(user.blogPosts[0].title === 'JS is Great');
-        done();
-      });
+    User.findOne({ name: 'Joe' }).populate('blogPosts').then((user) => {
+
+      assert(user.blogPosts[0].title === 'JS is Great');
+      done();
+    });
   });
+
+  it('saves a full relation graph', (done) => {
+
+    User.findOne({ name: 'Joe' })
+    .populate({
+      path: 'blogPosts',
+      populate: {
+        path: 'comments',
+        model: "comment",
+        populate: {
+          path: 'user',
+          model: 'user'
+        }
+      }
+    })
+    .then((user) => {
+      assert(user.name === 'Joe');
+      assert(user.blogPosts[0].title === 'JS is Great');
+      assert(user.blogPosts[0].comments[0].content === 'Congrats on a great post');
+      assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+      done();
+    });
+  }); 
 });  
